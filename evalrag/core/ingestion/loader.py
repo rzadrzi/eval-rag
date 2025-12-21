53881bb163225276eac7a02c9e526f4e22462227
Reza Darzi <darzi.reza@gmail.com>
2025-12-21 06:58:12 +0330

    feat: Add function that can detect file encoders and return utf-8

--- evalrag/core/ingestion/loader.py
+++ evalrag/core/ingestion/loader.py
@@ -1,7 +1,36 @@
 # loader.py
+import codecs
+import chardet
 from typing import List
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.documents import Document
+
+
+def document_encoding(path: str) -> str:
+    with open(path, "rb") as f:
+        raw = f.read(4096)  # Read a larger sample for better detection
+
+    # Check for BOM markers first
+    if raw.startswith(codecs.BOM_UTF16_LE):
+        return "utf-16-le"
+    elif raw.startswith(codecs.BOM_UTF16_BE):
+        return "utf-16-be"
+    elif raw.startswith(codecs.BOM_UTF16):
+        return "utf-16"
+    elif raw.startswith(codecs.BOM_UTF8):
+        return "utf-8-sig"
+    elif raw.startswith(codecs.BOM_UTF32_LE):
+        return "utf-32-le"
+    elif raw.startswith(codecs.BOM_UTF32_BE):
+        return "utf-32-be"
+
+    # Use chardet to detect encoding if no BOM is found
+    result = chardet.detect(raw)
+    encoding = result.get("encoding")
+    if encoding:
+        return encoding.lower()
+    # Return utf-8 if can not other encoding detected
+    return "utf-8"
 
 
 def pdf_loader(path) -> List[Document]:
